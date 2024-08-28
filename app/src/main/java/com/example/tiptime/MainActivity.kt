f62e051fe79f3db8f300f19b1b478892a83b49ad
Julia Sergeeva <attain06@gmail.com>
2024-08-28 22:43:07 +0300

    added keyboardOptions and keyBoardType next and done for both textfields

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -35,7 +35,6 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.getValue
@@ -43,6 +42,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -95,19 +95,26 @@ fun TipTimeLayout() {
         )
         EditNumberField(
             label = R.string.bill_amount,
+            keyboardOptions = KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Next,
+            ),
             value = amountInput,
             onValueChange = {amountInput = it},
-            modifier = Modifier
-                .fillMaxWidth()
+            modifier = Modifier.fillMaxWidth()
         )
         Spacer(modifier =Modifier.height(32.dp))
         EditNumberField(
             label = R.string.how_was_the_service,
+            keyboardOptions = KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Done
+            ),
             value = tipInput,
             onValueChange = {tipInput = it},
             modifier = Modifier
                 .fillMaxWidth()
-            )
+        )
                 Text(
                     text = stringResource(R.string.tip_amount, tip),
                     style = MaterialTheme.typography.displaySmall,
@@ -120,16 +127,16 @@ fun TipTimeLayout() {
 @Composable
 fun EditNumberField( //эта функция создает поле TextField
     @StringRes label: Int, //Аннотация @StringRes — это типобезопасный способ использования строковых ресурсов.
+    keyboardOptions: KeyboardOptions,
     value: String,
     onValueChange: (String) -> Unit,
     modifier: Modifier = Modifier) {
-
     TextField(
         value = value,
         onValueChange = onValueChange,
         singleLine = true,
         label = {Text(stringResource(label))},
-        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+        keyboardOptions = keyboardOptions,
         modifier = Modifier,
     )
 }
