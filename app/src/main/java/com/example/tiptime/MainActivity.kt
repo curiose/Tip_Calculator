419fcb16f0352f1bb232cfaaf63da1b687f3a7a0
Julia Sergeeva <attain06@gmail.com>
2024-08-28 15:13:17 +0300

    add state's observation (mutableStateOf)

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -33,6 +33,8 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -84,9 +86,10 @@ fun TipTimeLayout() {
 
 @Composable
 fun EditNumberField(modifier: Modifier = Modifier) {
+    var amountInput = mutableStateOf("0") //строка, потому что ввод через textfiled строковый элемент
     TextField(
-        value = "",
-        onValueChange = {},
+        value = amountInput.value,
+        onValueChange = {amountInput.value = it},
         modifier = Modifier,
     )
 }
